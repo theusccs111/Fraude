ed40af1618053aae04a6a4f4adfe815aa5bfcc56
Matheus Santana <theusccs111@gmail.com>
2024-07-12 12:29:22 -0300

    ajuste

---
+++ Fraude.Web/Controllers/IpController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fraude.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IpController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetIp()
+        {
+            var ipAddress = HttpContext.Connection.RemoteIpAddress?.ToString();
+            return Ok(new { ip = ipAddress });
+        }
+    }
+}
